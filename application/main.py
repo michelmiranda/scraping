bdad8ebd15424725ec315d3bf2c68921f9783244
Michel Miranda <micheldsmiranda@gmail.com>
2020-04-13 14:30:09 -0300

    Update main.py

--- application/main.py
+++ application/main.py
@@ -3,8 +3,8 @@ from controller import scrapeBook, getDownload
 
 if __name__ == "__main__":
 
-    owner = 'http://link.springer.com'
-    links = ['http://link.springer.com/openurl?genre=book&isbn=978-0-387-93837-0']
+    owner = 'http://link.domain.com'
+    links = ['http://link.domain.com/openurl?genre=book&isbn=978-0-387-93837-0']
     for link in links:
         Book = scrapeBook(link)
         getDownload(Book, owner)
